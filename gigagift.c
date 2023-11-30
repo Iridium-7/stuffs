725547ba4291c37dbbd72bea861775ab9ddd16f3
Iridium <152531571+Iridium-7@users.noreply.github.com>
2023-11-30 12:12:32 -0800

    gift for GigaCars, a friend of mine

---
+++ gigagift.c
@@ -0,0 +1,10 @@
+//might seem a little cringe but i have no other ideas
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello Giga!\n");
+    printf("I sent an EXE over to you because I could.\n");
+    printf("I will return 0 now.");
+    return 0;
+}
